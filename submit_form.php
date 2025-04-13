7ed1b058caebf1b029c38a7a0c9dc63c5a0851fa
Влад <vlad@MacBook-Air-Vlad.local>
2025-04-13 18:05:09 +0200

    Updated form

--- submit_form.php
+++ submit_form.php
@@ -1,10 +1,28 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = htmlspecialchars($_POST['name']);
-    $email = htmlspecialchars($_POST['email']);
-    $message = htmlspecialchars($_POST['message']);
+    // Odbieranie danych z formularza
+    $name = isset($_POST['name']) ? $_POST['name'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $course = isset($_POST['course']) ? $_POST['course'] : '';
 
-    // Здесь можно добавить код для отправки данных на почту или сохранения в базе данных.
-    echo "Dziękujemy za wiadomość, $name. Skontaktujemy się z Tobą wkrótce.";
+    // Adres e-mail, na który chcesz otrzymywać zgłoszenia
+    $to = "stepupspeakofficial@gmail.com";
+
+    // Temat wiadomości
+    $subject = "Nowe zgłoszenie na kurs";
+
+    // Treść wiadomości
+    $message = "Imię i nazwisko: $name\nE-mail: $email\nTelefon: $phone\nKurs: $course";
+
+    // Nagłówki (zawierają m.in. adres nadawcy)
+    $headers = "From: no-reply@twojastrona.pl";
+
+    // Wysyłanie wiadomości
+    if(mail($to, $subject, $message, $headers)) {
+        echo "Dziękujemy za zgłoszenie! Nasz manager skontaktuje się z Tobą w ciągu 24 godzin.";
+    } else {
+        echo "Wystąpił błąd przy wysyłaniu formularza. Proszę spróbować ponownie.";
+    }
 }
 ?>
